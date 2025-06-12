c2b9c880facfc66aee451706f8041ddde3ab18b9
waimea-twatkinson2 <twatkinson2@waimea.school.nz>
2025-06-12 12:37:37 +1200

    forogt to p[ushshahahh

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,7 @@
 
 from flask import Flask, render_template, request, flash, redirect
 import html
-import os
-from dotenv import load_dotenv
-import libsql_client
+
 from app.helpers.session import init_session
 from app.helpers.db import connect_db
 from app.helpers.errors import register_error_handlers, not_found_error
@@ -27,7 +25,18 @@ register_error_handlers(app)
 #-----------------------------------------------------------
 @app.get("/")
 def index():
-    return render_template("pages/home.jinja")
+    with connect_db() as client:
+        # Get all the things from the DB
+        sql = """
+            SELECT id, thing_to_do, timestamp, priority
+            FROM todo
+            ORDER BY priority DESC
+        """
+        result = client.execute(sql)
+        todo = result.rows
+
+        # And show them on the page
+        return render_template("pages/home.jinja", todo=todo)
 
 
 #-----------------------------------------------------------
@@ -39,64 +48,26 @@ def about():
 
 
 #-----------------------------------------------------------
-# Things page route - Show all the things, and new thing form
-#-----------------------------------------------------------
-@app.get("/things/")
-def show_all_things():
-    with connect_db() as client:
-        # Get all the things from the DB
-        sql = "SELECT id, name FROM things ORDER BY name ASC"
-        result = client.execute(sql)
-        things = result.rows
-
-        # And show them on the page
-        return render_template("pages/things.jinja", things=things)
-
-
-#-----------------------------------------------------------
-# Thing page route - Show details of a single thing
-#-----------------------------------------------------------
-@app.get("/thing/<int:id>")
-def show_one_thing(id):
-    with connect_db() as client:
-        # Get the thing details from the DB
-        sql = "SELECT id, name, price FROM things WHERE id=?"
-        values = [id]
-        result = client.execute(sql, values)
-
-        # Did we get a result?
-        if result.rows:
-            # yes, so show it on the page
-            thing = result.rows[0]
-            return render_template("pages/thing.jinja", thing=thing)
-
-        else:
-            # No, so show error
-            return not_found_error()
-
-
-#-----------------------------------------------------------
-# Route for adding a thing, using data posted from a form
+# Route for adding a task, using data posted from a form
 #-----------------------------------------------------------
 @app.post("/add")
 def add_a_thing():
     # Get the data from the form
-    name  = request.form.get("name")
-    price = request.form.get("price")
+    thing_to_do  = request.form.get("thing_to_do")
+    priority = request.form.get("priority")
 
     # Sanitise the inputs
     name = html.escape(name)
-    price = html.escape(price)
 
     with connect_db() as client:
-        # Add the thing to the DB
-        sql = "INSERT INTO things (name, price) VALUES (?, ?)"
-        values = [name, price]
+        # Add the task to the DB
+        sql = "INSERT INTO todo (thing_to_do, priority) VALUES (?, ?)"
+        values = [thing_to_do, priority]
         client.execute(sql, values)
 
         # Go back to the home page
-        flash(f"Thing '{name}' added", "success")
-        return redirect("/things")
+        flash(f"Task '{thing_to_do}' added", "success")
+        return redirect("/")
 
 
 #-----------------------------------------------------------
@@ -113,5 +84,4 @@ def delete_a_thing(id):
         # Go back to the home page
         flash("Thing deleted", "warning")
         return redirect("/things")
-
 
